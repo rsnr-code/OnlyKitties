da5335106c1c60bd379b8ea5b342e129ca2d1a1e
rsnr-code <rishadnero@gmail.com>
2022-09-19 17:43:52 -0400

    added homepage with router and controller

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv')
+const mongoose = require("mongoose");
+const connectDB = require('./config/db')
+const logger = require("morgan");
+const mainRoutes = require('./routes/main')
+
+// use .env file in config folder
+dotenv.config({path: './config/.env'});
+
+//Connect To Database
+connectDB();
+
+//Using EJS for views
+app.set("view engine", "ejs");
+
+//Logging
+app.use(logger("dev"));
+
+app.use('/', mainRoutes)
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}!`)
+})
