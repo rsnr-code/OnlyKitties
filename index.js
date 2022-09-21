dc24d4da66134686ebfa0e6be62607a6b2ba6d48
rsnr-code <rishadnero@gmail.com>
2022-09-21 16:13:50 -0400

    added google login capability

--- index.js
+++ index.js
@@ -4,16 +4,39 @@ const dotenv = require('dotenv')
 const mongoose = require("mongoose");
 const connectDB = require('./config/db')
 const logger = require("morgan");
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require("connect-mongo");
+
 const mainRoutes = require('./routes/main')
+const authRoutes = require('./routes/auth');
 
 // use .env file in config folder
 dotenv.config({path: './config/.env'});
+
+// Passport config
+require("./config/passport")(passport);
 
 //Connect To Database
 connectDB();
 
 //Using EJS for views
 app.set("view engine", "ejs");
+
+// Session middleware
+app.use(
+    session({
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: false,
+      store: MongoStore.create({mongoUrl: process.env.MONGO_URI,}),
+    })
+  ) 
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 
 //Static Folder
 app.use(express.static("public"));
@@ -22,6 +45,7 @@ app.use(express.static("public"));
 app.use(logger("dev"));
 
 app.use('/', mainRoutes)
+app.use('/auth', authRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}!`)
